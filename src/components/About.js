7c7750949fa6f6c464fd8f7575582330dde95a6c
N00rAhmed <tronn232003@gmail.com>
2023-10-25 17:28:22 +0100

    added icons

--- src/components/About.js
+++ src/components/About.js
@@ -2,6 +2,33 @@ import React from "react";
 import "../styles/About.css";
 import styled from 'styled-components';
 import Fade from 'react-reveal/Fade';
+
+import { FaReact } from 'react-icons/fa'
+import { IoLogoJavascript } from 'react-icons/io'
+import { IoLogoNodejs } from "react-icons/io";
+import { FaPython } from "react-icons/fa";
+import { AiOutlineHtml5 } from "react-icons/ai";
+import { FaCss3Alt } from "react-icons/fa";
+import { VscJson } from "react-icons/vsc";
+import { SiMongodb } from "react-icons/si";
+import { SiCsharp } from "react-icons/si";
+import { SiMicrosoftsqlserver } from "react-icons/si";
+import { SiMysql } from "react-icons/si";
+
+// SiSqlite
+// BsFiletypeSql
+// SiMysql
+// SiMicrosoftsqlserver
+// SiDotnet
+// TbBrandCSharp
+// SiDotnet
+// SiMongodb
+// BsFiletypeJson
+// FaCss3Alt
+// AiOutlineHtml5
+// FaPython
+// IoLogoJavascript
+
 function About() {
     return (
         <Container>
@@ -22,18 +49,31 @@ function About() {
                     
                     <Tech>Here are the technologies that I know how to use:</Tech>
 
-                    
+
                     <Fade bottom>
                         
                         <div className="about-content">
                             <div className="about-description">
 
                             <ul className="tech-stack">
-                                {tech_stack.map(function (tech_item, i) {
+
+                                <IoLogoJavascript style={{ height: "50px", width: "50px", backgroundColor: "#063970"}} />
+                                <FaReact style={{height: "50px", width: "50px"}} />
+                                <IoLogoNodejs style={{height: "50px", width: "50px"}} />
+                                <FaPython style={{height: "50px", width: "50px"}} />
+                                <AiOutlineHtml5 style={{height: "50px", width: "50px"}} />
+                                <FaCss3Alt style={{height: "50px", width: "50px"}} />
+                                <VscJson style={{height: "50px", width: "50px"}} />
+                                <SiMongodb style={{height: "50px", width: "50px"}} />
+                                <SiCsharp style={{height: "50px", width: "50px"}}/>
+                                <SiMicrosoftsqlserver style={{height: "50px", width: "50px"}}/>
+                                <SiMysql style={{height: "50px", width: "50px"}}/>
+
+                                {/* {tech_stack.map(function (tech_item, i) {
                         return (
                         <li>{tech_item}</li>
                         );
-                        })}
+                        })} */}
                             </ul>
                         </div>
                         </div>
@@ -79,7 +119,7 @@ function About() {
   const Interest = styled.p`
   width:35%;
   font-size:20px;
-  padding-top: 4%;
+  padding-top: 2%;
   `
     const tech_stack = [
       "Javascript",
